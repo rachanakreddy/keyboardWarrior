7f00e3a395fe9824705b8b4f82e22d75adf779b6
rachanakreddy <rachanakreddy@gmail.com>
2023-07-10 13:16:18 -0400

    spotify.js

--- client/spotify.js
+++ client/spotify.js
@@ -1,10 +1,12 @@
 
-const getAuthToken = async () => {
-    const res = await fetch(`/spotify/authtoken`, {
-        method: "GET"
-    })
+async function getAuthToken(){
+    const res = await fetch(`/spotify/authtoken`, {method: "GET"});
     let token = await res.json();
     return token.access_token;
-};
+}
 
+async function searchTrack(search){
+    let authToken = await getAuthToken();
+    //const res = await
+}
 
