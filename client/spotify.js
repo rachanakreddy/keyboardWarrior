08a55c38f79889d1aa430785d435d2967d86375f
rachanakreddy <rachanakreddy@gmail.com>
2023-07-11 14:50:07 -0400

    added a spotify util function

--- client/spotify.js
+++ client/spotify.js
@@ -1,12 +1,14 @@
 
 async function getAuthToken(){
-    const res = await fetch(`/spotify/authtoken`, {method: "GET"});
+    const res = await fetch('/spotify/authtoken', {method: "GET"});
     let token = await res.json();
     return token.access_token;
 }
 
 async function searchTrack(search){
     let authToken = await getAuthToken();
-    //const res = await
+    const res = await fetch('/spotify/authtoken', {method: "GET"});
+    
 }
+
 
