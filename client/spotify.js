6bb7cad8257160f69d46610ed2f00775e5206d32
rachanakreddy <rachanakreddy@gmail.com>
2023-07-12 14:50:53 -0400

    added crud

--- client/spotify.js
+++ client/spotify.js
@@ -5,10 +5,23 @@ async function getAuthToken(){
     return token.access_token;
 }
 
+
+
 async function searchTrack(search){
     let authToken = await getAuthToken();
-    const res = await fetch('/spotify/authtoken', {method: "GET"});
-    
+    const res = await fetch(`/spotify/gettrack`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            'token': authToken,
+            'quer': quer,
+            'offset': offset
+        })
+    });
+
+    let track = await res.json();
 }
 
 
